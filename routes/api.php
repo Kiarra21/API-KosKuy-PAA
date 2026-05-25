84def78bd19d318695d9c2bd06463dc504557619
Anwar <91013638+Fg120@users.noreply.github.com>
2026-05-25 23:27:15 +0700

    Add forgot & reset password endpoints

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
 	Route::post('/register', [AuthController::class, 'register']);
 	Route::post('/login', [AuthController::class, 'login']);
+	Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+	Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 	Route::middleware('auth:api')->group(function () {
 		Route::get('/me', [AuthController::class, 'me']);
 		Route::post('/logout', [AuthController::class, 'logout']);
